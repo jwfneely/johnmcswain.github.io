86b75abddefb9a3d576a25eb7e63616f72ea638a
John McSwain <john.i.mcswain@gmail.com>
2015-05-31 22:17:18 -0400

    Adding a simple search bar to execute a search query

--- InstagramExample/js/main.js
+++ InstagramExample/js/main.js
@@ -1,7 +1,20 @@
 var access_token;
 (function (){
     
-    access_token = window.location.hash.split("=")[1];
-    alert(access_token);
+    access_token = window.location.hash.split('=')[1];
+   // alert(access_token);
+    
 })();
 
+
+$('#searchButton').click(function(){
+  $.ajax({
+  url: 'https://api.instagram.com/v1/tags/search?q='+$('#searchText').val()+'&access_token='+access_token,
+  cache: false
+})
+  .done(function(response) {
+    console.log(response);
+  });  
+    
+    
+});
