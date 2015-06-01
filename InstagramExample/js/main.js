e3363595c2a7efa82d55f2277f67456ef4b8a0ab
John McSwain <john.i.mcswain@gmail.com>
2015-05-31 21:44:53 -0400

    Correcting URI to be exactly the url provided

--- InstagramExample/js/main.js
+++ InstagramExample/js/main.js
@@ -1,4 +1,4 @@
 //https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=e18b246fb6874ee3a337820c756652c4
 
 
-window.location = "https://instagram.com/oauth/authorize/?client_id=e18b246fb6874ee3a337820c756652c4&redirect_uri=http://johnmcswain.github.io/InstagramExample/&response_type=token";
+window.location = "https://instagram.com/oauth/authorize/?client_id=e18b246fb6874ee3a337820c756652c4&redirect_uri=http://johnmcswain.github.io/InstagramExample/index.html&response_type=token";
