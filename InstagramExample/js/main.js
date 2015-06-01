5c954f6d6b1a2d68c9c0da09ec612e00e94f4bf4
John McSwain <john.i.mcswain@gmail.com>
2015-05-31 23:22:26 -0400

    Pulling popular, including videos

--- InstagramExample/js/main.js
+++ InstagramExample/js/main.js
@@ -10,11 +10,17 @@ $('#searchButton').click(function(){
     $.ajax({
         type: "GET",
         dataType: "jsonp",
-        url: 'https://api.instagram.com/v1/tags/'+$('#searchText').val()+'/media/recent?access_token='+access_token+'&COUNT=1000', 
+        url: 'https://api.instagram.com/v1/media/popular?access_token='+access_token+'&COUNT=1000',
+        //'https://api.instagram.com/v1/tags/'+$('#searchText').val()+'/media/recent?access_token='+access_token+'&COUNT=1000', 
         success: function(response) {
             //console.log(response);
             $.each(response.data,function(index,value){
-                $('#resultDiv').append('<div class="results"><img src=\''+value.images.low_resolution.url+'\'></div>');
+                if(value.type == "image"){
+                    $('#resultDiv').append('<div class="results"><img src=\''+value.images.low_resolution.url+'\'></div>');
+                }else{
+                    $('#resultDiv').append('<div class="results"><video width="480" height="480" controls><source src=\''+value.images.low_resolution.url+'\' type=\'video/mp4\'></div>');
+
+                }
             });
             
             $('.results').addClass('border left');
