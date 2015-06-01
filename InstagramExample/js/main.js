fa516656cbacf83463c22569d62bc3e93b12a68f
John McSwain <john.i.mcswain@gmail.com>
2015-05-31 21:38:38 -0400

    Adding first instagram code...

---
+++ InstagramExample/js/main.js
@@ -0,0 +1,4 @@
+//https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=e18b246fb6874ee3a337820c756652c4
+
+
+$(window).location = "https://instagram.com/oauth/authorize/?client_id=e18b246fb6874ee3a337820c756652c4&redirect_uri=REDIRECT-URI&response_type=token";
