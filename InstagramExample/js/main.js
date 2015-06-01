20937e67b9dcb1aa36e5edf3feecb93db4971940
John McSwain <john.i.mcswain@gmail.com>
2015-05-31 22:02:14 -0400

    Changing URL parameter to parse the first hash value

--- InstagramExample/js/main.js
+++ InstagramExample/js/main.js
@@ -1,17 +1,1 @@
-function GetURLParameter(sParam)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam)
-        {
-            return sParameterName[1];
-        }
-
-    }
-
-}
-
-alert(GetURLParameter('#access_token'));
+alert(window.location.hash;);
