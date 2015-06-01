4bceb0ecc92fea5f496fd44d8cf36ab746497c6c
John McSwain <john.i.mcswain@gmail.com>
2015-06-01 00:23:59 -0400

    Adding additional button

--- InstagramExample/js/main.js
+++ InstagramExample/js/main.js
@@ -1,25 +1,32 @@
 var access_token;
+var isPopular = false;
 (function (){
     access_token = window.location.hash.split('=')[1];
     //alert(access_token);
 })();
 
+$('#popularButton').click(function(){
+    isPopular = true;
+    instagramRESTCall();
+});
 
 $('#searchButton').click(function(){
+    isPopular = false;
+    instagramRESTCall();
+})
+
+function instagramRESTCall(){
     $('#resultDiv').html(""); //clear the div
     $.ajax({
         type: "GET",
         dataType: "jsonp",
-        url: 'https://api.instagram.com/v1/media/popular?access_token='+access_token+'&COUNT=1000',
-        //'https://api.instagram.com/v1/tags/'+$('#searchText').val()+'/media/recent?access_token='+access_token+'&COUNT=1000', 
+        url: isPopular ?'https://api.instagram.com/v1/media/popular?access_token='+access_token:'https://api.instagram.com/v1/tags/'+$('#searchText').val()+'/media/recent?access_token='+access_token,
         success: function(response) {
-            //console.log(response);
             $.each(response.data,function(index,value){
                 if(value.type == "image"){
                     $('#resultDiv').append('<div class=\'results\'><img src=\''+value.images.low_resolution.url+'\'></div>');
                 }else{
-                    $('#resultDiv').append('<div class=\'results\'><video style=\'max-width:90%;max-height:90%\' controls><source src=\''+value.videos.low_resolution.url+'\' type=\'video/mp4\'></video></div>');
-
+                    $('#resultDiv').append('<div class=\'results\'><video style=\'max-width:90%;max-height:90%\'><source src=\''+value.videos.low_resolution.url+'\' type=\'video/mp4\'></video></div>');
                 }
             });
             
@@ -30,4 +37,5 @@ $('#searchButton').click(function(){
             //$('.results video').addClass('mejs-mediaelement');
         }
     });
-})
+    
+}
