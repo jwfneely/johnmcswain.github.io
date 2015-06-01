7f976a8030f6d313e7c42ada106dbcde5c3c247c
John McSwain <john.i.mcswain@gmail.com>
2015-05-31 22:42:05 -0400

    JSONP the response

--- InstagramExample/js/main.js
+++ InstagramExample/js/main.js
@@ -9,11 +9,18 @@ var access_token;
 
 $('#searchButton').click(function(){
   $.ajax({
+      /*
   url: 'https://api.instagram.com/v1/tags/'+$('#searchText').val()+'/media/recent?access_token='+access_token
 })
   .done(function(response) {
     console.log(response);
   });  
-    
+    */
+      type: "GET",
+            dataType: "jsonp",
+            url: 'https://api.instagram.com/v1/tags/'+$('#searchText').val()+'/media/recent?access_token='+access_token, 
+            success: function(data) {
+                console.log(data);
+            }
     
 });
