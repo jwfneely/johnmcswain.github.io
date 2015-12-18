204db5d45a8ebed466146837f6f2f6b2eccd745b
John McSwain <john.i.mcswain@gmail.com>
2015-12-18 01:08:42 -0500

    Resetting the code

--- js/grayscale.js
+++ js/grayscale.js
@@ -29,17 +29,13 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-
 // Google Maps Scripts
 // When the window has finished loading create our google map below
-//google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
-
-  //newDate.getFullYear();
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-/*
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 15,
@@ -52,7 +48,7 @@ function init() {
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map.
+        // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -163,19 +159,15 @@ function init() {
             }]
         }]
     };
-*/
 
-
-    // Get the HTML DOM element that will contain your map
+    // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
-    //var mapElement = document.getElementById('map');
+    var mapElement = document.getElementById('map');
 
     // Create the Google Map using out element and options defined above
-    //var map = new google.maps.Map(mapElement, mapOptions);
-
+    var map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    /*
     var image = 'img/map-marker.png';
     var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
     var beachMarker = new google.maps.Marker({
@@ -183,5 +175,4 @@ function init() {
         map: map,
         icon: image
     });
-    */
 }
